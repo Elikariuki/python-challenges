56818c7d107f3e1a18b667a916c8d0cdf4926956
elikariuki <ngugidoc4000@gmail.com>
2018-05-03 12:19:11 -0800

    if statement tutorial

---
+++ ifstate.py
@@ -0,0 +1,7 @@
+number=input("Think of a number any number and write it down\n>>")
+if int(number) % 2 == 1:
+    print("this is an odd number")
+elif int(number) % 2 ==  0:
+    print("this is an even number")   
+else:
+    print("input a natural number")     
